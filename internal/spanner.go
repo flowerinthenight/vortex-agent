3ae89e82fe1afcfffba7ee278a3bb5289d48adc1
flowerinthenight <root@flowerinthenight.com>
2025-08-30 16:29:03 +0900

    ci skip: cleanup

--- internal/spanner.go
+++ internal/spanner.go
@@ -2,22 +2,11 @@ package internal
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
-
-	spanner "cloud.google.com/go/spanner"
 )
-
-func NewSpannerClient(ctx context.Context, db string) (*spanner.Client, error) {
-	client, err := spanner.NewClient(ctx, db)
-	if err != nil {
-		return nil, err
-	}
-	return client, nil
-}
 
 type SpannerPayload struct {
 	Table string   `json:"table"`
